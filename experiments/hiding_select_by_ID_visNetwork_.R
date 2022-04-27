567a7a83d1036bc46e43da52444580123d36a9f0
gustavoborba13 <gustavolemosborba13@gmail.com>
2022-04-27 15:37:52 +0200

    experiment for hiding selectBy ID tab

---
+++ experiments/hiding_select_by_ID_visNetwork_.R
@@ -0,0 +1,139 @@
+server <- function(input, output) {
+  output$id <- renderText({             #output text stating ID (if exists) or error message
+    if (input$num %in% nodes.net$id){
+      print(input$num)
+    }else{
+      print("ID NOT FOUND")             #message if id is not found
+    }
+  })
+  
+  
+  output$mynetwork <- renderVisNetwork({
+    
+    ##date filter
+    #filtering edges that are outside date range
+    date.filtered.edges <- edges.net %>%
+      slice(c(which(edges.net$date <= input$date)))
+    
+    #filtering nodes that are not in  filtered edge data
+    date.filtered.nodes <- nodes.net %>%
+      slice(c(which(nodes.net$id %in% c(date.filtered.edges$from, date.filtered.edges$to))))
+    
+    if (input$num %in% nodes.net$id){
+      
+      ## connected nodes
+      # Create adjacency mappings
+      grp1 <- split(date.filtered.edges, date.filtered.edges$from) 
+      d1 <- lapply(grp1, function(x) x$to)
+      
+      grp2 <- split(date.filtered.edges, date.filtered.edges$to)
+      d2 <- lapply(grp2, function(x) x$from)
+      
+      d <- c(d1, d2)            #list with nodes and all its connections
+      rm(d1, d2,grp1, grp2)     #remove unnecessary objects from environment
+      
+      node <- input$num         #Selected node, find all nodes in the same component
+      seen <- c()               #Will contain all nodes in the component
+      next_d <- node            #Nodes in next order of neighbors
+      
+      while (length(next_d) > 0) {
+        # Nodes in current level
+        this_d <- next_d
+        # Nodes in next level, i.e. neighbours of nodes in current level 
+        next_d <- c()  
+        
+        # Go through nodes in current level
+        for (v in this_d) {
+          # If node hasn't been visited
+          if (!(v %in% seen)) {
+            # Add the node
+            seen <- unique(c(seen, v))
+            # And add its neighbours to search through next iteration 
+            next_d <- unique(c(next_d, d[[v]]))
+          }
+        }
+      }
+      
+      #filtered list of connected edges that are inside date range
+      list.connected.edges <- date.filtered.edges[c(date.filtered.edges$from,
+                                                    date.filtered.edges$to) %in% seen,]
+      
+      #filtered list of connected nodes that are inside date range
+      list.connected.nodes <- nodes.net %>%
+        slice(c(which(nodes.net$id %in% c(list.connected.edges$from,
+                                          list.connected.edges$to))))
+      
+      
+      
+      #Building the Visualization
+      visNetwork(nodes = list.connected.nodes, 
+                 edges =list.connected.edges, 
+                 main = "Trygg-Hansa", 
+                 background = NA) %>%
+        visIgraphLayout() %>%
+        visNodes( 
+          color = list(highlight = NA),
+          borderWidthSelected = 3) %>%
+        visEdges(color = "#0D0D0D", 
+                 hoverWidth = 5, 
+                 selectionWidth = 3.5, 
+                 shadow =TRUE)%>%
+        visInteraction(dragNodes = TRUE, 
+                       multiselect =TRUE, 
+                       navigationButtons = FALSE, 
+                       zoomView =TRUE) %>%
+        visOptions(highlightNearest = list(enabled =TRUE, 
+                                           degree = nrow(edges.net), 
+                                           hover =TRUE),
+                   nodesIdSelection = list(enabled = TRUE, 
+                                           useLabels = FALSE, 
+                                           selected = node,
+                                           style = 'width: 0px; 
+                                                    height: 0px; 
+                                                    background: NA; 
+                                                    color: NA')) %>% 
+        visLegend(useGroups = FALSE,
+                  addNodes= legend, 
+                  width = 0.08)
+      
+      
+      
+    }else{
+      visualization <- visNetwork(nodes = date.filtered.nodes, 
+                                  edges= date.filtered.edges, 
+                                  main = "Trygg-Hansa", 
+                                  background = NA) %>%
+        visIgraphLayout() %>%
+        visNodes( 
+          color = list(highlight = NA),
+          borderWidthSelected = 3) %>%
+        visEdges(color = "#0D0D0D",
+                 hoverWidth = 5,
+                 selectionWidth = 3.5,
+                 shadow =TRUE)%>%
+        visInteraction(dragNodes = TRUE,
+                       multiselect =TRUE, 
+                       navigationButtons = FALSE, 
+                       zoomView =TRUE) %>%
+        visOptions(highlightNearest = list(enabled =TRUE, 
+                                           degree = nrow(edges.net), 
+                                           hover =TRUE, 
+                                           hideColor = 'rgba(0,0,0,0)'),
+                   nodesIdSelection = list(enabled = TRUE, 
+                                           useLabels = FALSE,
+                                           style = 'width: 0px; 
+                                               height: 0px;
+                                               background: #f8f8f8;
+                                               color: black')) %>% 
+        visLegend(useGroups = FALSE,
+                  addNodes= legend, 
+                  width = 0.08)
+    }
+    
+  })
+  
+  
+}
+
+## Shiny App ----------------------------
+shinyApp(ui, server)
